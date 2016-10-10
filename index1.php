c9ee298b6463e87464a2a790de23dd1fcd9b94af
xen0rd <zhekrougjx@gmail.com>
2016-10-10 17:16:44 +0800

    INITIAL PUSH

---
+++ index1.php
@@ -0,0 +1,16 @@
+<?php
+ob_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=pages/CustLogin.php">
+<title>SB Admin 2</title>
+<script language="javascript">
+    window.location.href = "pages/CustLogin.php"
+</script>
+</head>
+<body>
+Go to <a href="pages/CustLogin.php"></a>
+</body>
+</html>
