f10836f9113987e98b8e2d3e408c29db37cea183
Dean Manalo <dean.anderson.manalo@gmail.com>
2016-10-31 19:41:39 +0800

    Added bootstrap and datatables

--- pages/inventory_details.php
+++ pages/inventory_details.php
@@ -7,7 +7,6 @@
 	<meta charset="UTF-8">
 	<title>La Leona Online Resort Reservation</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
 	<link rel="stylesheet" type="text/css" href="css/jquery.dataTables.min.css">
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 	<link rel="stylesheet" href="table.css" type="text/css">
@@ -16,7 +15,6 @@
 	
 	<script src="js/jquery-3.1.1.min.js"></script>
 	<script src="js/bootstrap.js"></script>
-	<script src="js/jquery-ui.js"></script>
 	<script src="js/jquery.dataTables.min.js"></script>
 
 </head>
